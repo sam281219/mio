8d0a4feb56837410e9ba7288da68d6169ab750dc
sam281219 <samusic314@gmail.com>
2024-02-21 22:20:57 +0100

    prueba 99

---
+++ Hola/src/main/java/h/hola/Hola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package h.hola;
+
+/**
+ *
+ * @author samus
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
